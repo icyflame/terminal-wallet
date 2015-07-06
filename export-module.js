c167697d26700243fe4362114a98e2ac24c4e57e
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-06 15:41:56 +0530

    Better formatting in the export-module, change filename of exported file
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- export-module.js
+++ export-module.js
@@ -1,14 +1,34 @@
 'use strict';
-function createExportFile () {
+function exportToFile (exportFilepath, printToConsole) {
   var fs = require('fs');
   var xdgBasedir = require('xdg-basedir');
   var logSymbols = require('log-symbols');
-  var contents = fs.readFileSync(xdgBasedir.data + '/wallet/expenses.csv');
-  var exportFilename = xdgBasedir.data + '/wallet/export.csv';
-  require('./setup-files.js')(exportFilename);
-  fs.writeFileSync(exportFilename, 'Date,Remarks,Category,Credit,Debit\n');
-  fs.appendFileSync(exportFilename, contents);
-  console.log('\n' + logSymbols.success + ' Your file can be found at ' + exportFilename + '\n');
+
+  var expensesFilepath = xdgBasedir.data + '/wallet/expenses.csv';
+  var contents = fs.readFileSync(expensesFilepath);
+
+  var prettyDate = require('date-format').asString('yyyy-MM-dd', new Date());
+
+  var defaultExportFilepath = xdgBasedir.data +
+                              '/wallet/exported/export-' +
+                              prettyDate + '.csv';
+
+  exportFilepath = exportFilepath ?
+                    exportFilepath : defaultExportFilepath;
+
+  fs.writeFileSync(exportFilepath, 'Date,Remarks,Category,Credit,Debit\n');
+  fs.appendFileSync(exportFilepath, contents);
+
+  printToConsole === undefined ?
+    console.log('\n' + logSymbols.success + ' Your file can be found at ' +
+                exportFilepath + '\n')
+    : undefined;
+
 }
 
-module.exports = createExportFile;
+function getHeaders () {
+  return 'Date,Remarks,Category,Credit,Debit\n';
+}
+
+exports.getHeaders = getHeaders;
+exports.exportToFile = exportToFile;
