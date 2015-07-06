7d7b852e1e9e8c3f13066db58391dc15bff77d29
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-06 12:32:02 +0530

    Prepare the entry script with appropriate switch-case
    - Check the number and type of arguments before writing an expense.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 'use strict';
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
+module.exports = function (input, opts) {
+  var clc = require('cli-color');
+
+  var expense_object = {
+    reason: input[2],
+    category: opts.c,
+    credit: '',
+    debit: ''
+  };
+
+  if (input[0] === 'credit' || input[0] === 'debit') {
+    if (input.length < 3 || typeof input[1] !== 'number') {
+      console.log(clc.red('Given input not enough cannot be parsed.'));
+      console.log('Use commands of the form: ' + clc.green('wallet debit 10 "Coffee"'));
+      process.exit(1);
+    }
   }
 
-  opts = opts || {};
-
-  return str + ' & ' + (opts.postfix || 'rainbows');
+  switch (input[0]) {
+    case 'debit':
+      expense_object.debit = input[1];
+      require('./file-module.js').writeExpense(expense_object);
+      break;
+    case 'credit':
+      expense_object.credit = input[1];
+      require('./file-module.js').writeExpense(expense_object);
+      break;
+    case 'stats':
+      break;
+    case 'balance':
+      break;
+    case 'export':
+      require('./export-module.js')();
+      break;
+    default:
+      console.log(clc.red('Not a valid option!'));
+      process.exit(1);
+  }
 };
