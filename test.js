e173741c11343c0f491548c0114596de49c37dfa
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-06 16:24:17 +0530

    Add one test.
    TODO: Add more tests.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- test.js
+++ test.js
@@ -2,7 +2,12 @@
 'use strict';
 var assert = require('assert');
 // var terminalWallet = require('./');
+var fileExists = require('file-exists');
+var xdgBasedir = require('xdg-basedir');
 
-it('should ', function () {
-  assert.strictEqual(true, true);
+var expensesFilepath = xdgBasedir.data + '/wallet/expenses.csv';
+
+it('should create files', function () {
+  require('./setup-files.js');
+  assert.strictEqual(fileExists(expensesFilepath), true);
 });
