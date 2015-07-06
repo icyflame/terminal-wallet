cb2b3d5fdb3deeb975a2a2f23ed1c00fbd7c1599
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-06 15:40:11 +0530

    Make setup-files a universal module
    - This can be called anytime to setup all the files and folders required
      by the module.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- setup-files.js
+++ setup-files.js
@@ -1,17 +1,16 @@
 'use strict';
 
 function createFileIfDoesNotExist (filepath) {
-  var fileExists = require('file-exists');
   var touch = require('touch');
   var xdg = require('xdg-basedir');
+  var mkdirp = require('mkdirp');
 
   filepath = filepath ? filepath : xdg.data + '/wallet/expenses.csv';
 
-  if (!fileExists(filepath)) {
-    var mkdirp = require('mkdirp');
-    mkdirp(xdg.data + '/wallet');
-    touch(filepath);
-  }
+  mkdirp.sync(xdg.data + '/wallet/exported');
+  mkdirp.sync(xdg.data + '/wallet/closed');
+  touch.sync(xdg.data + '/wallet/expenses.csv');
+
 }
 
 module.exports = createFileIfDoesNotExist;
