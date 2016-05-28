d1527c8e24f7d4b2b1bb4c55940b488974b78fee
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-05-28 19:58:32 +0530

    Add some more ava tests for other functions
    - add tests that work for debit/credit

--- test.js
+++ test.js
@@ -1,26 +1,49 @@
 /* global t */
 import test from 'ava';
 
+var atmIn = ["credit", 3000, "From ATM"];
+
 test('files are created', t => {
-	var fileExists = require('file-exists');
-	var xdgBasedir = require('xdg-basedir');
-	var expensesFilepath = xdgBasedir.data + '/wallet/expenses.csv';
-	require('./setup-files.js');
-	return fileExists(expensesFilepath);
+  var fileExists = require('file-exists');
+  var xdgBasedir = require('xdg-basedir');
+  var expensesFilepath = xdgBasedir.data + '/wallet/expenses.csv';
+  require('./setup-files.js');
+  return fileExists(expensesFilepath);
 });
 
 test('wrong date is not allowed', t => {
-	var module = require('./');
+  var module = require('./');
 
-	var input = ["credit", 3000, "From ATM"];
-	var opts = {};
+  module(atmIn, {});
 
-	module(input, opts);
+  var input = ["debit", 50, "Coffee and mufin - one more time"];
+  var opts = {d: "2015-5-5"};
 
-	input = ["debit", 50, "Coffee and mufin - one more time"];
-	opts = {d: "2015-5-5"};
+  t.throws(function() {
+    module(input, opts);
+  });
+});
 
-	t.throws(function() {
-		module(input, opts);
-	}, "Given date is not formatted as yyyy-mm-dd");
+test('wrong debit/credit is not allowed', t => {
+  var module = require('./');
+
+  module(atmIn, {});
+
+  var input = ["debit", 50];
+
+  t.throws(function() {
+    module(input, {});
+  });
+});
+
+test('wrong stash transactions are not allowed', t => {
+  var module = require('./');
+
+  module(atmIn, {});
+
+  var input = ["stash"];
+
+  t.throws(function() {
+    module(input, {});
+  });
 });
